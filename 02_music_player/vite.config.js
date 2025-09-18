2d2373c2e035433615a214cd97cc6603fb98f855
Your Name <you@example.com>
2025-09-18 11:44:57 +0500

    done

--- 02_music_player/vite.config.js
+++ 02_music_player/vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const projectName = path.basename(__dirname)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),    tailwindcss(),],
+   base: `/Rhombix-Technologies/${projectName}/`, // ✅ works for any folder
 })
